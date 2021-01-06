015289a9662886b477f95f7a1eb1dd16549b23ed
Stringbao <string_bao@163.com>
2021-01-06 17:04:12 +0800

    3

--- src/main/model/core/Model.js
+++ src/main/model/core/Model.js
@@ -1,7 +1,11 @@
 
 export default class BaseModel{
     constructor(){
-        
+        this._parent = null;   
+    }
+
+    setParent(parent){
+        this._parent = parent;   
     }
 
     get(){
